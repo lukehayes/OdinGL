bf7a3eb6585b035188b50ffaaf5b3d91f4b95a50
lukehayes <lukepurplehayes@gmail.com>
2024-03-14 10:18:28 +0000

    Renamed aPos to attr_position

--- assets/shaders/passthru-vsh.glsl
+++ assets/shaders/passthru-vsh.glsl
@@ -1,10 +1,10 @@
 #version 330 core
-layout (location = 0) in vec3 aPos;
+layout (location = 0) in vec3 attr_position;
   
 out vec4 vertexColor;
 
 void main()
 {
-    gl_Position = vec4(aPos, 1.0);
-    vertexColor = vec4(0.5, 0.5, 0.5, 1.0);
+    gl_Position = vec4(attr_position, 1.0);
+    vertexColor = vec4(1.0, 1.0, 1.0, 1.0);
 }
